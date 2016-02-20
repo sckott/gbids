91e70c6d6d5776e8e36c1657e364009fdff567f8
Scott Chamberlain <myrmecocystus@gmail.com>
2016-02-20 08:50:54 -0800

    added random route

--- README.md
+++ README.md
@@ -5,13 +5,14 @@ Right now setup to load Genbank accession numbers into Redis, and provides an AP
 
 ## Under the hood
 
-Configuration might change, but right now using [webdis](http://webd.is/), a JSON API to a running Redis instance. Using Ruby/Sinatra to make the API itself. Accession id's are stored directly in Redis, in memory. Haven't sorted out yet whether we can actually store all ~650 million or so identifiers, and then there's the GI numbers as well, could be a different route on the API. If we can store all data in memory, this should be super fast, if not, will pursue another approach.
+Using Ruby/Sinatra to make the API itself. Accession id's are stored directly in Redis, in memory. Haven't sorted out yet whether we can actually store all ~650 million or so identifiers, and then there's the GI numbers as well, could be a different route on the API. If we can store all data in memory, this should be super fast, if not, will pursue another approach.
 
 ## API Endpoints
 
 * `/` re-routes to `/heartbeat`
 * `/heartbeat` - list routes
 * `/match/:id,:id,...` - only `GET` for now. submit many accession numbers, get back boolean (match or no match)
+* `/random?n=` get a set of random accession numbers
 
 ## start
 
@@ -19,12 +20,6 @@ start Redis
 
 ```
 redis-server
-```
-
-start Webdis
-
-```
-webdis &
 ```
 
 Load data
@@ -38,12 +33,13 @@ sh dataprep.sh 100
 where the `100` is the number of keys (accession numbers) to input into Redis. Tried up to 10 million so far on my macbook pro with 8 GB, works fine. Dumping to disk from 10 million keys gives ~ 164 mb `dump.rdb` file
 
 ```
-hub clone sckott/gbcheck
-cd gbcheck
+hub clone sckott/gbcheck && cd gbcheck
 unicorn -p 8888
 ```
 
 ## Example
+
+using [curl](https://curl.haxx.se/) and [jq](https://stedolan.github.io/jq/)
 
 ```
 curl 'http://localhost:8888/heartbeat' | jq .
@@ -56,7 +52,7 @@ curl 'http://localhost:8888/heartbeat' | jq .
 ```
 
 ```
-curl 'http://localhost:8888/match/AACY024124486,AACY024124483,asdfd,asdf,AACY024124476'
+curl 'http://localhost:8888/match/AACY024124486,AACY024124483,asdfd,asdf,AACY024124476' | jq .
 #> {
 #>   "AACY024124486": true,
 #>   "AACY024124483": true,
@@ -77,6 +73,17 @@ curl -XPOST 'http://localhost:8888/match' -F ids='AACY024124486,AACY024124483,as
 #> }
 ```
 
+```
+curl 'http://localhost:8888/random' | jq .
+#> {
+#>   "AACY024124486": true,
+#>   "AACY024124483": true,
+#>   "asdfd": false,
+#>   "asdf": false,
+#>   "AACY024124476": true
+#> }
+```
+
 ## todo
 
-* probably dont need webdis, just fun to play with
+* ...
--- api.rb
+++ api.rb
@@ -1,22 +1,15 @@
 require 'rubygems'
 require 'sinatra'
-require 'faraday'
 require "multi_json"
 require "sinatra/multi_route"
+require 'redis'
 #require File.join File.dirname(__FILE__), "utils_fxns"
+
+$redis = Redis.new host: ENV.fetch('REDIS_PORT_6379_TCP_ADDR', 'localhost'),
+                   port: ENV.fetch('REDIS_PORT_6379_TCP_PORT', 6379)
 
 class FBApp < Sinatra::Application
   register Sinatra::MultiRoute
-
-  # before do
-  #   puts '[Params]'
-  #   p params
-  # end
-
-  # before do
-  #   puts '[env]'
-  #   p env
-  # end
 
   not_found do
     halt 400, {'Content-Type' => 'application/json'}, MultiJson.dump({ 'error' => 'an error occurred' })
@@ -26,17 +19,13 @@ class FBApp < Sinatra::Application
     halt 404, {'Content-Type' => 'application/json'}, MultiJson.dump({ 'error' => 'route not found' })
   end
 
-  not_found do
-    halt 414, {'Content-Type' => 'application/json'}, MultiJson.dump({ 'error' => 'URL too long, reduce number of IDs or use POST' })
-  end
-
   error 500 do
     halt 500, {'Content-Type' => 'application/json'}, MultiJson.dump({ 'error' => 'server error' })
   end
 
   before do
     headers "Content-Type" => "application/json; charset=utf8"
-    headers "Access-Control-Allow-Methods" => "HEAD, GET"
+    headers "Access-Control-Allow-Methods" => "HEAD, GET, POST"
     headers "Access-Control-Allow-Origin" => "*"
     cache_control :public, :must_revalidate, :max_age => 60
   end
@@ -47,16 +36,22 @@ class FBApp < Sinatra::Application
   end
 
   get '/' do
-    redirect '/heartbeat'
+    redirect '/heartbeat', 301
   end
 
   get "/heartbeat/?" do
     return MultiJson.dump({
       "routes" => [
-        "/match/:identifier",
+        "/match/:identifier (GET)",
+        "/match (POST)",
+        "/random?n=",
         "/heartbeat"
       ]
     })
+  end
+
+  get '/random/?' do
+    get_random
   end
 
   get '/match/:ids' do
@@ -64,31 +59,21 @@ class FBApp < Sinatra::Application
   end
 
   post '/match/?' do
-    match_id_post
+    match_id
   end
 
   # helpers
   def match_id
-    ids = params[:ids].gsub(',', '/')
-    conn = Faraday.new(:url => 'http://127.0.0.1:7379/MGET/' + ids)
-    res = conn.get
-    matches = MultiJson.load(res.body)['MGET'].map { |x| !x.nil? }
+    ids = params[:ids].split(',')
+    out = $redis.mget(ids)
+    matches = out.map { |x| !x.nil? }
     res = Hash[params[:ids].split(',').zip matches]
     return MultiJson.dump(res)
   end
 
-  def match_id_post
-    #puts params[:ids]
-    ids = 'MGET/' + params[:ids].gsub(',', '/')
-    #puts 'MGET/' + ids
-    conn = Faraday.new(:url => 'http://127.0.0.1:7379')
-    res = conn.post do |req|
-      req.headers['Content-Type'] = 'text/plain'
-      req.body = ids
-    end
-    matches = MultiJson.load(res.body)['MGET'].map { |x| !x.nil? }
-    #puts matches
-    res = Hash[params[:ids].split(',').zip matches]
+  def get_random
+    n = params[:n] || 10
+    res = n.to_i.times.collect { $redis.randomkey }
     return MultiJson.dump(res)
   end
 
