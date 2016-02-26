d081fa60203d5a3b2b4192434fd9c28e65b761fc
Scott Chamberlain <myrmecocystus@gmail.com>
2016-02-25 23:22:32 -0800

    swap out ruby for awk in file prep

--- dataprep.sh
+++ dataprep.sh
@@ -1,18 +1,27 @@
 #/bin/bash
 
 n=${1-1000}
-echo ">> Setting output to $n - pass a number to change it"
+echo ">> Setting output to $n (default: 1000)"
 
-echo ">> clipping data subset"
-head -n $n GbAccList.0207.2016 > gbsmall.txt
-echo ">> in Ruby, preparing data to pipe to Redis"
-ruby dataprep.rb
+echo ">> clipping data subset, then awking"
+head -n $n GbAccList.0207.2016 \
+	| cut -d, -f1,3 \
+	| awk '{print "SET,"$0}' \
+	| awk -F"," -v quote="\"" -v OFS="\" \"" '$1=$1 {print "\x22" $0 "\x22"}' \
+	> accdat.txt
+
+awk '{print $1,$3,$2}' accdat.txt > gidat.txt
+
 echo '>> unix2dos'
 unix2dos accdat.txt gidat.txt
+
 echo '>> piping into Redis'
 cat accdat.txt | redis-cli -p 6379 --pipe
 cat gidat.txt | redis-cli -p 6380 --pipe
+
 echo '>> cleaning up'
 rm gbsmall.txt accdat.txt gidat.txt
 
 echo ">>$n IDs now in Redis :)"
+
+echo '>> done'
